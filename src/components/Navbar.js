f8e17f53191d932ccaaa8fe57adc544b8736a9df
vmatteo81 <103205571+vmatteo81@users.noreply.github.com>
2022-06-19 07:45:24 +0200

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,6 +15,24 @@ class Navbar extends Component {
           <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" />
           &nbsp; ReCrypto
         </a>
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; Market
+        </a>
+        <a
+          className="navbar-brand col-sm-3 col-md-2 mr-0"
+          href="http://www.dappuniversity.com/bootcamp"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          &nbsp; Docs
+        </a>
 
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
